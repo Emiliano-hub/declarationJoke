deda26611994faf955267e5b9ec15e529c57a0ed
EMI <emiliano.2.farias@hotmail.com>
2021-05-29 11:05:30 -0300

    Messages added and btnYes logic

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
+alert('Hola, como estas?');
+alert('Desde hace mucho eh querido preguntarte esto');
+alert('Me ah costado mucho valor decirte esto, pero...');
+alert('Te gustaria salir en una cita conmigo?');
+
+let btnYes = document.getElementById('btn-yes');
 let btnNo = document.getElementById('btn-no');
 let container = document.getElementById('container');
 
 btnNo.addEventListener('mousemove', hadlerBtnNo);
+btnYes.addEventListener('click', hadlerBtnYes);
+
+if (screen.width === 500) {
+  btnNo.removeEventListener('mousemove', hadlerBtn);
+  btnNo.addEventListener('click', hadlerBtn);
+}
 
 let x;
 let y;
@@ -13,6 +25,11 @@ let btnHeight = btnNo.offsetHeight;
 
 let maxSpaceWidht = spaceWidth - btnWidth;
 let maxSpaceHeight = spaceHeight - btnHeight;
+
+function hadlerBtnYes() {
+  alert('Enserio!!, Oh si, presentia que dirias que si');
+  alert('Ya te contacto por via mail');
+}
 
 function hadlerBtnNo() {
   positionRandom();
