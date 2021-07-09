65f231496771a790de39f4a83d32d083c786d1a7
EMI <emiliano.2.farias@hotmail.com>
2021-07-09 11:24:24 -0300

    Version 2.0 finished with class added

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-// import Swal from 'sweetalert2';
-
 let btn1 = document.getElementById('Principal-button');
 let btnYes = document.getElementById('btn-yes');
 let btnNo = document.getElementById('btn-no');
 let container = document.getElementById('container');
-
-// btnNo.addEventListener('mousemove', hadlerBtnNo);
-// btnYes.addEventListener('dblclick', handlerBtnYes);
+let x, y;
+let maxSpaceHeight, maxSpaceWidht;
 
 // if (screen.width === 500) {
 //   btnNo.removeEventListener('mousemove', hadlerBtnNo);
@@ -17,29 +14,26 @@ let container = document.getElementById('container');
 
 class Logic {
   constructor() {
-    // this.initialize = this.initialize.bind(this); /// Ka?
     this.initialize();
-    this.getMaxSizes();
   }
 
   initialize() {
     this.btnNo = btnNo;
     this.btnYes = btnYes;
     this.btn1 = btn1;
-    // this.message1();
-    // this.activeEvents = this.activeEvents.bind(this);
+    console.log(this);
+    this.maxSpaceWidht = maxSpaceWidht;
+    this.maxSpaceHeight = maxSpaceHeight;
     this.activeFirstButton();
-    // this.hadlerBtnNo = this.hadlerBtnNo.bind(this);
-    // this.message2 = this.message2.bind(this);
   }
 
   activeFirstButton() {
-    this.btn1.addEventListener('click', this.firstMessage);
+    this.btn1.addEventListener('click', this.firstMessage.bind(this));
     this.activeChoices();
   }
 
   firstMessage() {
-    // this.message1();
+    this.message1();
     btn1.style.opacity = 0;
     setTimeout(() => {
       btn1.style.display = 'none';
@@ -47,13 +41,28 @@ class Logic {
   }
 
   activeChoices() {
-    this.btnNo.addEventListener('click', this.hadlerBtnNo);
-    this.btnYes.addEventListener('click', this.message2);
+    this.btnNo.addEventListener('mousemove', this.hadlerBtnNo.bind(this));
+    this.btnYes.addEventListener('dblclick', this.message2);
 
     // if (screen.width === 500) {
     //   this.btnNo.removeEventListener('mousemove', this.hadlerBtnNo);
     //   this.btnNo.addEventListener('click', this.hadlerBtnNo);
     // }
+  }
+
+  hadlerBtnNo() {
+    this.getMaxSizes();
+    this.positionRandom();
+    console.log(this.x, this.y);
+    this.btnNo.style.left = this.x + 'px';
+    this.btnNo.style.top = this.y + 'px';
+  }
+
+  positionRandom() {
+    this.x = Math.floor(Math.random() * (this.maxSpaceWidht - 0 + 1) + 0);
+    this.y = Math.floor(Math.random() * (this.maxSpaceHeight - 0 + 1) + 0);
+    console.log(this.x, this.y);
+    return x, y;
   }
 
   getMaxSizes() {
@@ -62,59 +71,37 @@ class Logic {
     let btnWidth = btnNo.offsetWidth;
     let btnHeight = btnNo.offsetHeight;
 
-    let maxSpaceWidht = spaceWidth - btnWidth;
-    let maxSpaceHeight = spaceHeight - btnHeight;
+    this.maxSpaceWidht = spaceWidth - btnWidth;
+    this.maxSpaceHeight = spaceHeight - btnHeight;
     return maxSpaceHeight, maxSpaceWidht;
   }
 
-  hadlerBtnNo() {
-    alert('test');
+  async message1() {
+    let message1 = await swal.fire('Hola, como estas?');
+    let message2 = await swal.fire(
+      'Desde hace mucho tiempo que te veo, escucho y hablo contigo y ya no puedo esconder estos sentimientos tan desbordantes que siento hacia vos...'
+    );
+    let message3 = await swal.fire(
+      'Fue dificil encontrar tales palabras para decirte lo que siento y representar a la vez mis sentimienos asi que despues de tanto reflexionar, tanto cuestionar, tome mi desicion.'
+    );
+    let message4 = await swal.fire(
+      'Me ah costado mucho poder decirte esto, pero...'
+    );
+    let message5 = await swal.fire('¿Te gustaria salir en una cita conmigo?');
+    let message6 = await swal.fire(
+      'Para tomar tu respuesta da doble click en Si o no'
+    );
   }
 
-  message1() {
-    alert('Hola, como estas?');
-    // alert('Desde hace mucho eh querido preguntarte esto');
-    // alert('Me ah costado mucho valor decirte esto, pero...');
-    // alert('Te gustaria salir en una cita conmigo?');
-    // alert('Para tomar tu respuesta da doble click en Si o no');
-  }
-
-  message2() {
-    alert('Enserio!!, Oh si, presentia que dirias que si');
-    alert('Ya te contacto por via mail');
+  async message2() {
+    let message1 = await swal.fire(
+      'Enserio!!, Oh si, presentia que dirias que si'
+    );
+    let message2 = await swal.fire('No sabes lo feliz que me siento 😊');
+    let message3 = await swal.fire('En breve me estaré contactando contigo');
   }
 }
 
 /////------CLASS CREATION-------
 
-let x, y;
-
-// let spaceWidth = container.offsetWidth;
-// let spaceHeight = container.offsetHeight;
-// let btnWidth = btnNo.offsetWidth;
-// let btnHeight = btnNo.offsetHeight;
-
-// let maxSpaceWidht = spaceWidth - btnWidth;
-// let maxSpaceHeight = spaceHeight - btnHeight;
-
-// function hadlerBtnYes() {
-//   alert('Enserio!!, Oh si, presentia que dirias que si');
-//   alert('Ya te contacto por via mail');
-// }
-
-// function hadlerBtnNo() {
-//   positionRandom();
-//   btnNo.style.left = x + 'px';
-//   btnNo.style.top = y + 'px';
-// }
-
-// function positionRandom() {
-//   x = Math.floor(Math.random() * (maxSpaceWidht - 0 + 1) + 0);
-//   y = Math.floor(Math.random() * (maxSpaceHeight - 0 + 1) + 0);
-//   return x, y;
-// }
-
-// window.logic = new Logic();
 const start = new Logic();
-// start = 1;
-// console.log(window.logic);
